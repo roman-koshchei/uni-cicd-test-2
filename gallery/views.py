6715772ac4158a145bd91b6a551c3a8370a90f9a
Roman Koshchei <romankoshchei@gmail.com>
2025-05-23 11:03:53 +0300

    views

--- gallery/views.py
+++ gallery/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from .models import Category, Image
+
+def gallery_view(request):
+    categories = Category.objects.prefetch_related('image_set').all()
+    return render(request, 'gallery.html', {'categories': categories})
+
+def image_detail(request, pk):
+    image = get_object_or_404(Image, pk=pk)
+    return render(request, 'image_detail.html', {'image': image})
